05c9bf907cd4e21b3626dfcb518d0e6118d16c38
NullByte3 <>
2024-08-20 17:10:42 +0300

    six

---
+++ module2/6six.py
@@ -0,0 +1,6 @@
+import random
+
+numbers = str(random.randint(0, 9)) + str(random.randint(0, 9)) + str(random.randint(0, 9))
+print("Arvottu luku on (0-9): " + numbers)
+numbers = str(random.randint(1, 6)) + str(random.randint(1, 6)) + str(random.randint(1, 6))+ str(random.randint(1, 6))
+print("Arvottu luku on (1-6): " + numbers)
