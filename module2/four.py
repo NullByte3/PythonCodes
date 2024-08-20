b07112147036b985bd26b43e46d61329ee2bd649
NullByte3 <>
2024-08-20 15:14:44 +0300

    joo

---
+++ module2/four.py
@@ -0,0 +1,6 @@
+num1 = int(input("Anna ensimmäinen luku: "))
+num2 = int(input("Anna toinen luku: "))
+num3 = int(input("Anna kolmas luku: "))
+
+print("Kaikki yhteensa: " + str(num1 + num2 + num3))
+print("keskiarvo: "+ str((num1+num2+num3)/3))
